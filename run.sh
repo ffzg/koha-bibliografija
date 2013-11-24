58fb839c3147bb91d0ce76724d1f043132b540a8
Dobrica Pavlinusic <dpavlin@rot13.org>
2013-11-24 14:37:31 +0100

    log STDERR better

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -e
 
 cd /srv/koha-bibliografija
-LC_COLLATE=hr_HR.utf8 KOHA_CONF=/etc/koha/sites/ffzg/koha-conf.xml ./html.pl 2>&1 | tee /tmp/koha-bibliografija.log
+( LC_COLLATE=hr_HR.utf8 KOHA_CONF=/etc/koha/sites/ffzg/koha-conf.xml ./html.pl 2>&1 ) | tee /tmp/koha-bibliografija.log
 
